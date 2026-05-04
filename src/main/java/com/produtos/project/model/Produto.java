12a630890c96922a4c1865ca60f1b72d0a38ed9c
José William <prof.williamfirmino@gmail.com>
2026-05-04 19:10:46 -0300

    first commit

---
+++ src/main/java/com/produtos/project/model/Produto.java
@@ -0,0 +1,25 @@
+package com.produtos.project.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of ="id")
+@Entity
+public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private Double preco;
+}
